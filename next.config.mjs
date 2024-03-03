431818ffdf1deed2b1d146d36f3af0a7078b0b5a
黃韻儒 <lalame888@gmail.com>
2024-03-03 21:46:23 +0800

    再切一下 為什麼cicd不會過啊

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'next export',
+  output: 'export',
   reactStrictMode: true,
   distDir: 'dist',
   basePath: '/minesweeper-react'
