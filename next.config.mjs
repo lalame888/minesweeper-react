0c1a94ce6b8b95493a8d34b4db4db9b9f19b2674
黃韻儒 <lalame888@gmail.com>
2024-03-03 20:37:39 +0800

    修改output next export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'next export',
   distDir: 'dist',
   basePath: '/minesweeper-react',
   assetPrefix: '/minesweeper-react/'
