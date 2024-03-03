6297e685ff8a37bd387697d686569b058504d471
黃韻儒 <lalame888@gmail.com>
2024-03-03 21:56:09 +0800

    拿掉output

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  distDir: 'dist',
+  //   distDir: 'dist',
   basePath: '/minesweeper-react'
 }
 
