f00d49ab6bece2bd20cb73b181fc587eb59027a1
黃韻儒 <lalame888@gmail.com>
2024-03-03 22:22:41 +0800

    加上assetPrefix 把另外一個yml先拿掉

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   distDir: 'dist',
-  basePath: '/minesweeper-react'
+  basePath: '/minesweeper-react',
+  assetPrefix: 'https://lalame888.github.io/minesweeper-react/'
 }
 
 export default nextConfig
